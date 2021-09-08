339d50f87d2b887190ea40f8792eedaced5153a8
ravindikumarasinghe <ravindikumarasinghe2@gmail.com>
2021-09-08 23:21:56 +0530

    Auth password validators

--- Blog_App/Blog_App/settings.py
+++ Blog_App/Blog_App/settings.py
@@ -126,3 +126,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+
+]
