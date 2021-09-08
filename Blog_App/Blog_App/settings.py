e6ad5618277eb401fe87608a6181f242182c5499
ravindikumarasinghe <ravindikumarasinghe2@gmail.com>
2021-09-08 23:22:28 +0530

    password validation

--- Blog_App/Blog_App/settings.py
+++ Blog_App/Blog_App/settings.py
@@ -131,5 +131,8 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
 
 ]
