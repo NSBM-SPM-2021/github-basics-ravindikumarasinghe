bfc8970bb6d374e41b37f3d5e4a3b5ae91e0c2da
ravindikumarasinghe <ravindikumarasinghe2@gmail.com>
2021-09-23 14:13:46 +0530

    Summary

--- Blog_App/settings.py
+++ Blog_App/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jv=q11&g93an88b8wb2*t61&fh@43cu(368b#lj^$io^9rm-x9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
